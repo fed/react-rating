81a1da507328b7da0918cfebff6931e2b8ab2a1d
Federico Knüssel <fknussel@gmail.com>
2017-09-22 15:59:12 -0300

    Polyfill Array#fill for IE

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Styleguide from './Styleguide';
+import 'array.prototype.fill'; // polyfill Array.prototype.fill for IE
 import './index.css';
 
 ReactDOM.render(
